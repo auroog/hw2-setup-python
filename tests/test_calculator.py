d1339a1d462da460ea8ca5ade210b7b538e95672
Aurora <ac2649@njit.edu>
2024-09-16 18:06:54 -0400

    Finalized hw2 setup with tests, pylint, and coverage

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,14 @@
+
+"""
+Unit test for the calculator function.
+"""
+
+
 def test_addition():
-	assert 1 +1 ==2
+    """Testing the add function"""
+    assert 1 + 1 == 2
+
+
 def test_subtraction():
-	assert 2 - 1 == 1
+    """Testing the subtract function"""
+    assert 2 - 1 == 1
